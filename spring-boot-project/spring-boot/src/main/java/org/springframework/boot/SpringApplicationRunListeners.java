698e5a4df61dd12a741b36b120bd30b4c15e5ad3
Young <cky13858806884@gmail.com>
2018-08-04 17:06:34 +0800

    int i

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListeners.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListeners.java
@@ -33,6 +33,7 @@ import org.springframework.util.ReflectionUtils;
  */
 class SpringApplicationRunListeners {
 
+	private  int i;
 	private final Log log;
 
 	private final List<SpringApplicationRunListener> listeners;
